0f6919fe29017e12ed44fd0960f8b6f6438f19a9
Jeffrey Lullen <lullenj@gmail.com>
2026-05-05 08:27:12 +0800

    Initial Commit

---
+++ WeatherAppCohort07/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeatherAppCohort07
+//
+//  Created by jeffrey lullen on 5/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeatherAppCohort07/WeatherAppCohort07App.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppCohort07App.swift
+//  WeatherAppCohort07
+//
+//  Created by jeffrey lullen on 5/5/26.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppCohort07App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
